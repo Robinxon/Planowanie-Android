0af17bf985c084cd776ef0a9f937909ad096b524
Robinxon <robinxon.pl@gmail.com>
2020-08-09 17:18:29 +0200

    dodanie obsługę wyświetlania i losowania atutu

--- app/src/main/java/com/example/planowanie/GameActivity.kt
+++ app/src/main/java/com/example/planowanie/GameActivity.kt
@@ -2,10 +2,22 @@ package com.example.planowanie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        imageViewAtut.setBackgroundResource(R.drawable.none)
+    }
+    
+    fun randomAtut() {
+        when((1..4).random()) {
+            1 -> imageViewAtut.setBackgroundResource(R.drawable.karo)
+            2 -> imageViewAtut.setBackgroundResource(R.drawable.kier)
+            3 -> imageViewAtut.setBackgroundResource(R.drawable.pik)
+            4 -> imageViewAtut.setBackgroundResource(R.drawable.trefl)
+        }
     }
 }
