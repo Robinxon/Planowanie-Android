ec86caec65a97f62690c8fac4c5e34e5f2afca24
Robinxon <robinxon.pl@gmail.com>
2020-08-09 15:15:11 +0200

    przygotowanie frontu głównego ekranu

--- app/src/main/java/com/example/planowanie/MainActivity.kt
+++ app/src/main/java/com/example/planowanie/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.planowanie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextPlayer3Name.visibility = View.GONE
+        editTextPlayer4Name.visibility = View.GONE
+
+        radioGroupPlayerCount.setOnCheckedChangeListener { _, checkedId ->
+            val radio: RadioButton = findViewById(checkedId)
+            when(radio.text) {
+                "2 graczy" -> {
+                    editTextPlayer3Name.visibility = View.GONE
+                    editTextPlayer4Name.visibility = View.GONE
+                }
+                "4 graczy" -> {
+                    editTextPlayer3Name.visibility = View.VISIBLE
+                    editTextPlayer4Name.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
