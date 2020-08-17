38a0bd975ea993300b53c25175973cac8ec0d126
Robinxon <robinxon.pl@gmail.com>
2020-08-17 23:33:41 +0200

    przygotowanie layoutu statystyk

--- app/src/main/java/com/example/planowanie/StatsActivity.kt
+++ app/src/main/java/com/example/planowanie/StatsActivity.kt
@@ -1,17 +1,52 @@
 package com.example.planowanie
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
+import androidx.core.view.updateMargins
+import kotlinx.android.synthetic.main.activity_stats.*
 
 class StatsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
         actionBar?.setDisplayHomeAsUpEnabled(true);
+
+        addPlayerStats()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
+
+    private fun addPlayerStats() {
+        //stałe
+        val dp8 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8F, resources.displayMetrics).toInt()
+
+        for (i in 1..20){
+            //player layout
+            val player1Layout = LinearLayout(this)
+            player1Layout.setPadding(dp8, dp8, dp8, dp8)
+            player1Layout.setBackgroundResource(R.drawable.tv_border)
+            val paramsLayout: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            paramsLayout.setMargins(0, 0, 0, dp8)
+            player1Layout.layoutParams = paramsLayout
+
+            //text view
+            val textPlayerName = TextView(this)
+            val paramsText: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            textPlayerName.layoutParams = paramsText
+            textPlayerName.setTextColor(resources.getColor(R.color.black))
+            textPlayerName.text = "test"
+
+            //dadanie do layoutu głównego
+            player1Layout.addView(textPlayerName)
+            usersStats.addView(player1Layout)
+        }
+    }
+
 }
