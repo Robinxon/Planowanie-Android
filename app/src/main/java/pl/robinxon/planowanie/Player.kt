e34ba5e3b69921b05fc333f89cc22e6395693a52
Robinxon <robinxon.pl@gmail.com>
2020-08-26 13:34:21 +0200

    zmiana zmiennych na nullable w Player.kt

--- app/src/main/java/pl/robinxon/planowanie/Player.kt
+++ app/src/main/java/pl/robinxon/planowanie/Player.kt
@@ -4,6 +4,6 @@ import java.io.Serializable
 
 class Player: Serializable {
     var points: Int = 0
-    var planned = IntArray(20) {-1}
-    var taken = IntArray(20) {-1}
+    var planned = arrayOfNulls<Int>(14)
+    var taken = arrayOfNulls<Int>(14)
 }
