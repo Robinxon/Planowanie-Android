427e7749f0d8d411b32b6d44c28dde3ed0e91d89
Robinxon <robinxon.pl@gmail.com>
2020-08-27 22:35:51 +0200

    zmiana przechowywania kart w rundach

--- app/src/main/java/pl/robinxon/planowanie/Game.kt
+++ app/src/main/java/pl/robinxon/planowanie/Game.kt
@@ -6,7 +6,7 @@ class Game: Serializable {
     var players = arrayOfNulls<Player>(5)
     var currentRound: Int = 1
     var currentPlayer: Int = 1
-    var currentCards: Int = 13
+    var currentCards = intArrayOf(0, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1 )
     var toDisabling: Int? = null
     var ended: Boolean = false
     var atuts = arrayOfNulls<Int>(17)
