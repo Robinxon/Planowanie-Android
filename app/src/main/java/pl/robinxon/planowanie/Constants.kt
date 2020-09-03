a9961c6b3a4e327f49f09b7034ad21f197d26eec
Robinxon <robinxon.pl@gmail.com>
2020-09-03 21:39:37 +0200

    zmiana stałych na release

--- app/src/main/java/pl/robinxon/planowanie/Constants.kt
+++ app/src/main/java/pl/robinxon/planowanie/Constants.kt
@@ -2,10 +2,10 @@ package pl.robinxon.planowanie
 
 object Constants {
     //release
-    //const val FIRE_MATCH: String = "match"
-    //const val FIRE_HISTORY: String = "history"
+    const val FIRE_MATCH: String = "match"
+    const val FIRE_HISTORY: String = "history"
 
     //prod
-    const val FIRE_MATCH: String = "match_prod"
-    const val FIRE_HISTORY: String = "history_prod"
+    //const val FIRE_MATCH: String = "match_prod"
+    //const val FIRE_HISTORY: String = "history_prod"
 }
