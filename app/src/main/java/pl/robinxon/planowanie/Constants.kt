3566eed19e607e5b1e66b64c3a03fdac9cfc9104
Robinxon <robinxon.pl@gmail.com>
2020-09-12 13:04:13 +0200

    dodanie stałej wersji i zmiana stałych na prod

--- app/src/main/java/pl/robinxon/planowanie/Constants.kt
+++ app/src/main/java/pl/robinxon/planowanie/Constants.kt
@@ -2,10 +2,12 @@ package pl.robinxon.planowanie
 
 object Constants {
     //release
-    const val FIRE_MATCH: String = "match"
-    const val FIRE_HISTORY: String = "history"
+    //const val FIRE_MATCH: String = "match"
+    //const val FIRE_HISTORY: String = "history"
+    //const val VERSION: String = BuildConfig.VERSION_NAME
 
     //prod
-    //const val FIRE_MATCH: String = "match_prod"
-    //const val FIRE_HISTORY: String = "history_prod"
+    const val FIRE_MATCH: String = "match_prod"
+    const val FIRE_HISTORY: String = "history_prod"
+    const val VERSION: String = BuildConfig.VERSION_NAME + " prod " + BuildConfig.VERSION_CODE
 }
